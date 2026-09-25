cb54a0e730e838944e788b59c0d12bee72eeafb0
Sylvain Saucier <62949224+sysaulab@users.noreply.github.com>
2026-09-25 09:07:19 -0700

    test web manifest

---
+++ sudoku/sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'sudoku-v1';
+const ASSETS = [
+  './', './index.html', './manifest.webmanifest', './icon.svg',
+  './icon-192.png', './icon-512.png', './icon-maskable-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil((async () => {
+    const c = await caches.open(CACHE);
+    await Promise.all(ASSETS.map(u => c.add(u).catch(() => {}))); // don't fail on a missing icon
+    self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || new URL(req.url).origin !== location.origin) return;
+  e.respondWith((async () => {
+    const hit = await caches.match(req);
+    if (hit) return hit;
+    try {
+      const res = await fetch(req);
+      if (res.ok) (await caches.open(CACHE)).put(req, res.clone());
+      return res;
+    } catch {
+      return (await caches.match('./index.html')) || Response.error();
+    }
+  })());
+});
